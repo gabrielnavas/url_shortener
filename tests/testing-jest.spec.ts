d8a1c5e5855efe31ccc5bc4b6fd2eeb532da5c3a
navas <gabrielnavas@email.com>
2023-07-19 15:24:18 -0300

    test: the hash must be a maximum of 7 characters

--- tests/testing-jest.spec.ts
+++ tests/testing-jest.spec.ts
@@ -1,5 +1,14 @@
-describe('Testing Jest', () => {
-  test('1, 2, 3 testing...', () => {
-    expect(1).toBe(1)
+class CreateRandomHash {
+  execute = () => {
+    return "1234567"
+  }
+}
+
+describe('Create Random Hash Class', () => {
+  test('the hash must be a maximum of 7 characters', () => {
+    const sut = new CreateRandomHash()
+    const hash = sut.execute()
+    expect(typeof hash).toEqual('string')
+    expect(hash.length).toEqual(7)
   })
 })
